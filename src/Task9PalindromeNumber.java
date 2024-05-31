8c824dc8512013a42a61aab1909288eec4291cb5
Owner <kirilovmail90@gmail.com>
2024-05-31 15:45:14 +0300

    first commit

---
+++ src/Task9PalindromeNumber.java
@@ -0,0 +1,45 @@
+/*
+Given an integer x, return true if x is a
+palindrome
+, and false otherwise.
+
+
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+ */
+
+import java.util.Scanner;
+
+public class Task9PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        String str = Integer.toString(num);
+        int length = str.length();
+        boolean isTrue = true;
+
+        for (int i = 0; i < length / 2; i++) {
+            if (str.charAt(i) != str.charAt(length - 1 - i)) {
+                isTrue = false;
+                break;
+            }
+        }
+
+        System.out.println(isTrue);
+    }
+}
